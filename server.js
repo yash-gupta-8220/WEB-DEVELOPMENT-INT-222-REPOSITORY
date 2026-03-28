fe8892ed496f113b441df660db280063c3420518
yash-gupta-8220 <yashcjcs560@gmail.com>
2026-03-28 20:00:33 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+let ideas = [];
+
+// open website
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+});
+
+// add idea
+app.post("/add", (req, res) => {
+    ideas.push(req.body);
+    res.send("Idea Added");
+});
+
+// get ideas
+app.get("/ideas", (req, res) => {
+    res.json(ideas);
+});
+
+app.listen(3000, () => {
+    console.log("Server running at http://localhost:3000");
+});
