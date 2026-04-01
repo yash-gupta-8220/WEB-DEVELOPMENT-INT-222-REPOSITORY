f0dd9276040798ba5d1325452f0bf7b0c06f0594
yash-gupta-8220 <yashcjcs560@gmail.com>
2026-04-01 23:18:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,47 +1,27 @@
 const express = require("express");
 const app = express();
 
-
-const http = require("http").createServer(app);
-const io = require("socket.io")(http);
-
-// middlewares
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-// 🔥 serve frontend
-app.use(express.static("public"));
+app.use(express.urlencoded({extended:true}));
 
 let ideas = [];
 
-
-app.get("/ideas", (req, res) => {
-  res.json(ideas);
+// open website
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
 });
 
+// add idea
 app.post("/add", (req, res) => {
-  const idea = req.body;
-  ideas.push(idea);
-
-  // 🔥 send to all users instantly
-  io.emit("newIdea", idea);
-
-  res.send("Idea Added");
+    ideas.push(req.body);
+    res.send("Idea Added");
 });
 
-io.on("connection", (socket) => {
-  console.log("User connected");
-
-  socket.on("chatMessage", (msg) => {
-    io.emit("chatMessage", msg); // broadcast
-  });
-
-  socket.on("disconnect", () => {
-    console.log("User disconnected");
-  });
+// get ideas
+app.get("/ideas", (req, res) => {
+    res.json(ideas);
 });
 
-//
-http.listen(process.env.PORT || 3000, () => {
-  console.log("Server running...");
+app.listen(3000, () => {
+    console.log("Server running at http://localhost:3000");
 });
