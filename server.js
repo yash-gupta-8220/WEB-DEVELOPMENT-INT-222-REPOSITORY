707b217d02f8f6c7b4b5bec445174abd94cd7263
yash-gupta-8220 <yashcjcs560@gmail.com>
2026-04-01 14:21:20 +0530

    Update server.js

--- server.js
+++ server.js
@@ -24,5 +24,5 @@ app.post("/add", (req, res) => {
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(Server running on port ${PORT});
+    console.log(`Server running on port ${PORT}`);
 });
