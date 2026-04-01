69e93dc4024b412f85f980a4a643570bcf82956d
yash-gupta-8220 <yashcjcs560@gmail.com>
2026-04-01 13:51:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -53,5 +53,5 @@ app.get("/ideas", async (req, res) => {
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(🚀 Server running on port ${PORT});
+  console.log(` Server running on port ${PORT}`);
 });
