cd249d4ea95f252614db115a6f47216b8238f0dc
yash-gupta-8220 <yashcjcs560@gmail.com>
2026-04-02 00:38:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,26 +2,30 @@ const express = require("express");
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
+
+// 🔥 ADD THIS LINE (VERY IMPORTANT)
+app.use(express.static(__dirname));
 
 let ideas = [];
 
 // open website
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/index.html");
+  res.sendFile(__dirname + "/index.html");
 });
 
 // add idea
 app.post("/add", (req, res) => {
-    ideas.push(req.body);
-    res.send("Idea Added");
+  ideas.push(req.body);
+  res.send("Idea Added");
 });
 
 // get ideas
 app.get("/ideas", (req, res) => {
-    res.json(ideas);
+  res.json(ideas);
 });
 
+// start server
 app.listen(3000, () => {
-    console.log("Server running at http://localhost:3000");
+  console.log("Server running at http://localhost:3000");
 });
