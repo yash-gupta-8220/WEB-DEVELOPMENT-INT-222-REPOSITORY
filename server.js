d9cd9f1c6d97bc057b94f8abc559d29bfa6909e2
yash-gupta-8220 <yashcjcs560@gmail.com>
2026-04-10 22:01:15 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const app = express();
 
 app.use(express.json());
 app.use(express.static(__dirname));
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/login.html");
+});
 
 // ===== SECRET =====
 const SECRET = process.env.SECRET || "mysecretkey";
