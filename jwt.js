81a62e173b8bf1484790e980019d847b4eb12224
yash-gupta-8220 <yashcjcs560@gmail.com>
2026-04-11 15:19:42 +0530

    Update jwt.js

--- jwt.js
+++ jwt.js
@@ -11,18 +11,19 @@ function createToken(user) {
     );
 }
 
-// 🔐 VERIFY TOKEN (MIDDLEWARE)
 function verifyToken(req, res, next) {
-  const token = req.headers.authorization?.split(" ")[1];
+    const authHeader = req.headers.authorization;
 
-    if (!token) {
+    if (!authHeader) {
         return res.status(401).json({ message: "No token provided" });
     }
 
+    const token = authHeader.split(" ")[1];
+
     try {
         const decoded = jwt.verify(token, SECRET);
-        req.user = decoded; // attach user info
-        next(); // continue
+        req.user = decoded;
+        next();
     } catch (err) {
         return res.status(403).json({ message: "Invalid token" });
     }
