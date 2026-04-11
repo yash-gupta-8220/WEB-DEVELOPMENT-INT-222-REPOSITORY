f03d196fae9c0958b091af5fdb0da02f2d31a6a8
yash-gupta-8220 <yashcjcs560@gmail.com>
2026-04-11 15:17:19 +0530

    Update jwt.js

--- jwt.js
+++ jwt.js
@@ -13,7 +13,7 @@ function createToken(user) {
 
 // 🔐 VERIFY TOKEN (MIDDLEWARE)
 function verifyToken(req, res, next) {
-    const token = req.headers["authorization"];
+  const token = req.headers.authorization?.split(" ")[1];
 
     if (!token) {
         return res.status(401).json({ message: "No token provided" });
