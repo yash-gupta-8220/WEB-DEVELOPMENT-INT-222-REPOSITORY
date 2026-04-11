0db6a622a267fcc8a1bfb4c1aa8f3ec8d9305f5d
yash-gupta-8220 <yashcjcs560@gmail.com>
2026-04-11 20:43:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -31,38 +31,3 @@ app.get("/ideas", (req, res) => {
 app.listen(3000, () => {
   console.log("Server running on port 3000");
 });
-
-
-const express = require("express");
-const fs = require("fs");
-const app = express();
-
-app.use(express.json());
-app.use(express.static(__dirname)); // serve frontend
-
-
-let ideas = [];
-
-if (fs.existsSync("data.json")) {
-  ideas = JSON.parse(fs.readFileSync("data.json"));
-}
-
-
-app.post("/add", (req, res) => {
-  ideas.push(req.body);
-
-  // SAVE TO FILE
-  fs.writeFileSync("data.json", JSON.stringify(ideas, null, 2));
-
-  res.json({ message: "Idea saved successfully" });
-});
-
-
-app.get("/ideas", (req, res) => {
-  res.json(ideas);
-});
-
-
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
-});
