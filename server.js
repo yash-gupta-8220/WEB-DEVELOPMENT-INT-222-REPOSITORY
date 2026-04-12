3f44106a780fbc394d60718f744e4e19b85a417a
yash-gupta-8220 <yashcjcs560@gmail.com>
2026-04-12 11:18:55 +0530

    Update server.js

--- server.js
+++ server.js
@@ -22,20 +22,6 @@ app.post("/add", (req, res) => {
   res.json({ message: "Idea saved successfully" });
 });
 
-app.put("/update/:index", (req, res) => {
-    const index = req.params.index;
-
-    if (index >= 0 && index < ideas.length) {
-        ideas[index] = req.body;
-
-        // Save updated data to file
-        fs.writeFileSync("data.json", JSON.stringify(ideas, null, 2));
-
-        res.json({ message: "Idea updated successfully" });
-    } else {
-        res.status(404).json({ message: "Idea not found" });
-    }
-});
 
 
 app.get("/ideas", (req, res) => {
